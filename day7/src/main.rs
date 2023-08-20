6444b90fde88464e82c3468ef8ad68e10d171418
Tuan Tran <tropicaldog17@gmail.com>
2023-08-20 07:58:47 +0700

    WIP: day 7 AoC 2022

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,88 +1,83 @@
-use std::collections::{HashMap, HashSet, LinkedList};
-#[derive(Debug, PartialEq, Hash, Eq)]
-struct Name(String);
-struct FileSystem{
-    current_directory: Directory,
-    directories: HashMap<Name, Directory>,
-    files: HashSet<File>,
+use camino::{Utf8PathBuf, Utf8Path};
+use nom::{IResult, Finish};
+use nom::combinator::all_consuming;
+use nom::sequence::{preceded, separated_pair};
+use nom::{
+    bytes::complete::{take_while1,tag},
+    combinator::{map},
+    branch::alt
+
+};
+
+fn parse_path(i: &str) -> IResult<&str, Utf8PathBuf> {
+    map(
+        take_while1(|c: char| "abcdefghijklmnopqrstuvwxyz./".contains(c)),
+        Into::into,
+    )(i)
 }
-struct Directory{
-    parent: Name,
-    name: Name,
-    dirs: HashMap<Name, Directory>,
-    files: HashMap<Name, File>,
-    size: u64
-}
-impl Directory{
-    pub fn new(parent: Name, name: Name) -> Self{
-        Self{ parent, name, dirs: HashMap::new(), files: HashMap::new(), size: 0}
-    }
-}
-struct File{
-    size: u64,
-    name: Name,
-    parent: Name,
-}
-impl File{
-    pub fn new(name: &Name, parent: &Name, size: u64) -> Self{
-        Self { size, name: *name, parent: *parent }
-    }
+#[derive(Debug)]
+struct Ls;
+fn parse_ls(i: &str) -> IResult<&str, Ls>{
+    map(tag("ls"), |_| Ls)(i)
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
+struct Cd(Utf8PathBuf);
+
+fn parse_cd(i: &str) -> IResult<&str, Cd> {
+    map(preceded(tag("cd "), parse_path), Cd)(i)
+}
+#[derive(Debug)]
 enum Command{
-    ChangeDirectory(Name),
-    List,
+    Ls,
+    Cd(Utf8PathBuf)
 }
-fn parse_command(i: &str) -> Option<Command>{
-    let args = i.split_whitespace().collect::<Vec<_>>();
-    match *args.get(0).unwrap(){
-        "$" => {
-            match *args.get(1).unwrap(){
-                "cd" => Some(Command::ChangeDirectory(Name(args.get(2).unwrap().to_string()))),
-                "ls" => Some(Command::List),
-                _ => unimplemented!()
-            }
-        },
-        _ => None
+impl From<Ls> for Command{
+    fn from(value: Ls) -> Self {
+        Command::Ls
     }
 }
-fn parse_ls_line_info(i: &str, curr: &mut Directory) {
-    let args = i.split_whitespace().collect::<Vec<_>>();
-    match *args.get(0).unwrap(){
-        "dir" => {
-            let name = Name(String::from(*args.get(1).unwrap()));
-            curr.dirs.insert(name, Directory::new(curr.name, name));
-        },
-        s => {
-            match s.parse::<u64>(){
-                Ok(size) => {
-                    let file_name = Name(String::from(*args.get(1).unwrap()));
-                    curr.files.insert(file_name, File::new(file_name, curr.name, size));
-                },
-                Err(e) => println!("Expect line format to be <size> <filename>, got {:?}", args),
-            }
-        }
+impl From<Cd> for Command{
+    fn from(value: Cd) -> Self {
+        Command::Cd(value.0)
     }
+}
+fn parse_command(i: &str) -> IResult<&str, Command> {
+    let (i, _) = tag("$ ")(i)?;
+    alt((map(parse_ls, Into::into), map(parse_cd, Into::into)))(i)
+}
+#[derive(Debug)]
+enum Entry{
+    Dir(Utf8PathBuf),
+    File(u64, Utf8PathBuf)
+}
+fn parse_entry(i: &str) -> IResult<&str, Entry>{
+    let file = map(
+        separated_pair(nom::character::complete::u64, tag(" ") ,parse_path), |(size, path)| Entry::File(size, path), 
+    );
+    let dir = map(
+        preceded(tag("dir "), parse_path),
+        Entry::Dir
+    );
+    alt((file, dir))(i)
+}
+#[derive(Debug)]
+enum Line{
+    Entry(Entry),
+    Command(Command)
+}
+fn parse_line(i: &str) -> IResult<&str, Line>{
+    alt(((map(parse_command, Line::Command)), map(parse_entry, Line::Entry)))(i)
 }
 fn main() {
-    for line in include_str!("input.txt").lines(){
-        println!("{line}");
+    let lines = include_str!("input.txt").lines().map(|l| {
+        all_consuming(parse_line)(l).finish().unwrap().1
+    }).collect::<Vec<_>>();
+    for line in lines{
+        println!("{line:?}");
     }
 }
 #[cfg(test)]
 mod test{
-    use crate::{parse_command, Command, Name};
-
-    #[test]
-    fn test_parse_command_cd(){
-        assert_eq!(parse_command("$ cd /"), Some(Command::ChangeDirectory(Name("/".to_string()))));
-        assert_eq!(parse_command("$ cd .."), Some(Command::ChangeDirectory(Name("..".to_string()))));        
-        assert_eq!(parse_command("$ cd abc"), Some(Command::ChangeDirectory(Name("abc".to_string()))));
-        // assert_eq!(parse_command("$ ef ."), None);
-    }
-    #[test]
-    fn test_parse_command_ls(){
-        assert_eq!(parse_command("$ ls"), Some(Command::List));
-    }
+    
 }
